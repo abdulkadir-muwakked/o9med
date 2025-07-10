89780134420d4175b011272cb108230834c1f0e7
abdulkadir-muwakked <abdulkader.mouket@gmail.com>
2025-07-10 09:35:41 +0300

    metadataBase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://o9med-rjb2.vercel.app"),
   title: ``,
   description: `.`,
   openGraph: {
