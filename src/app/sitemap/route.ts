8ec670ffca196e55310393d120e928437ee5ea7e
abdulkadir-muwakked <abdulkader.mouket@gmail.com>
2025-07-10 06:44:53 +0300

    getAllPosts

---
+++ src/app/sitemap/route.ts
@@ -0,0 +1,18 @@
+import { getAllPosts } from "@/lib/api";
+
+export async function GET() {
+  const baseUrl = "https://o9medical.com";
+  const posts = getAllPosts();
+
+  const urls = posts.map(
+    (post) => `  <url>\n    <loc>${baseUrl}/posts/${post.slug}</loc>\n    <changefreq>weekly</changefreq>\n    <priority>0.8</priority>\n  </url>`
+  );
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n  <url>\n    <loc>${baseUrl}/</loc>\n    <changefreq>weekly</changefreq>\n    <priority>1.0</priority>\n  </url>\n${urls.join("\n")}\n</urlset>`;
+
+  return new Response(sitemap, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
+}
